5162150710706f44ca872e2173a118e41d3b8270
Sam <samcope24@gmail.com>
2022-05-07 10:50:55 +0100

    add starting program

--- SingleResponsibility/Program.cs
+++ SingleResponsibility/Program.cs
@@ -4,21 +4,32 @@
     {
         public static void Main()
         {
-            StandardMessages.WelcomeMessage();
+            System.Console.WriteLine("Welcome to my Application");
 
-            Person user = PersonDataCapture.Capture();
+            Person user = new Person();
 
-            bool isUserValid = PersonValidator.Validate(user);
+            System.Console.Write("What is your first name?");
+            user.FirstName = System.Console.ReadLine();
 
-            if (isUserValid == false)
+            System.Console.Write("What is your last name?");
+            user.LastName = System.Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(user.FirstName))
             {
-                StandardMessages.EndApplication();
+                System.Console.WriteLine("You did not give us a valid first name!");
+                System.Console.ReadLine();
                 return;
             }
 
-            AccountGenerator.CreateAccount(user);
+            if (string.IsNullOrWhiteSpace(user.LastName))
+            {
+                System.Console.WriteLine("You did not give us a valid last name!");
+                System.Console.ReadLine();
+                return;
+            }
 
-            StandardMessages.EndApplication();
+            System.Console.WriteLine($"Your username is { user.FirstName.Substring(0,1) }{ user.LastName }");
+            System.Console.ReadLine();
         }
     }
 }
