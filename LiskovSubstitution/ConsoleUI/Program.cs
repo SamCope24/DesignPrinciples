f78bd3ab48357f11647057591db7fb3ef22690a4
Sam <samcope24@gmail.com>
2022-05-09 14:15:57 +0100

    add liskov starting project

---
+++ LiskovSubstitution/ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using LiskovLibrary;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager accountingVP = new Manager();
+
+            accountingVP.FirstName = "Emma";
+            accountingVP.LastName = "Stone";
+            accountingVP.CalculatePerHourRate(4);
+
+            Employee emp = new CEO();
+
+            emp.FirstName = "Tim";
+            emp.LastName = "Corey";
+            emp.AssignManager(accountingVP);
+            emp.CalculatePerHourRate(2);
+
+            Console.WriteLine($"{ emp.FirstName }'s salary is ${ emp.Salary }/hour.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LiskovSubstitution/README.md
@@ -0,0 +1,3 @@
+## Liskov Substitution Principle
+
+- if S is a subtype of T then object of T may be replaced with object of type S without breaking the program
